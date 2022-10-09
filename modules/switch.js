51d365eb40c61c2e72b728bda5fdbc2249cf53c2
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2022-10-09 09:40:35 +0100

    last update

--- modules/switch.js
+++ modules/switch.js
@@ -5,17 +5,13 @@ export default class Switch {
       const allSections = document.querySelectorAll('.sections');
       const listCont = document.querySelector('.nav-items');
       listCont.addEventListener('click', (e) => {
-        e.preventDefault();
-        const { id } = e.target.dataset;
-        if (id) {
-        // Removing the active section
-          allSections.forEach((section) => {
-            section.classList.remove('activesec');
-          });
-          //  Adding the active section
-          const targetedSec = document.getElementById(id);
-          targetedSec.classList.add('activesec');
+        if (e.target.dataset) {
+          allSections.forEach(section => {
+            section.classList.remove();
+          })
         }
+        const target = document.getElementById(e.target.dataset);
+        target.classList.add();
       });
     }
-}
+} 
